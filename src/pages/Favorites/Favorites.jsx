7b47b72d5548912571f238eb165b258123ee71e9
nayon117 <hasibulhasannayon260@gmail.com>
2023-09-24 06:43:34 +0600

    add see more and see less functionality to favorite page

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,11 +1,51 @@
- 
+import { useEffect, useState } from "react";
+import FavoritesCard from "./FavoritesCard";
 
 const Favorites = () => {
-    return (
-        <div>
-            favorits
+  const [favorites, setFavorites] = useState([]);
+  const [noFound, setNoFound] = useState("");
+  const [isShow,setIsShow] = useState(false)
+  useEffect(() => {
+    const favoritesItems = JSON.parse(localStorage.getItem("favorits"));
+    if (favoritesItems) {
+      setFavorites(favoritesItems);
+    } else {
+      setNoFound("No data found");
+    }
+  }, []);
+  const handleRemove = () => {
+    localStorage.clear()
+    setFavorites([])
+    setNoFound("No data found");
+  }
+
+  return (
+    <div>
+      {noFound ? (
+        <p className="h-[70vh] flex justify-center items-center text-2xl font-bold">
+           
+          {noFound}
+        </p>
+      ) : (
+          <div>
+            {favorites.length>0 && <button onClick={handleRemove} className="px-3 py-2 my-4 bg-red-500 rounded-md text-white block mx-auto">Delete All Favorites</button>}
+          <div className="grid grid-cols-2 gap-5">
+              {
+                isShow? favorites.map((phone) => (
+                  <FavoritesCard key={phone.id} phone={phone}></FavoritesCard>
+                )) : 
+                favorites.slice(0,4).map((phone) => (
+                  <FavoritesCard key={phone.id} phone={phone}></FavoritesCard>
+                ))
+            }
+            </div>
+            <button onClick={() => setIsShow(!isShow)} className="px-3 my-4 py-2 bg-red-500 rounded-md text-white block mx-auto">
+               {isShow ? "see less" : "see more"}
+            </button>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Favorites;
--- src/pages/Favorites/FavoritesCard.jsx
+++ src/pages/Favorites/FavoritesCard.jsx
@@ -1,31 +1,10 @@
-import PropTypes from "prop-types";
-import swal from "sweetalert";
-const PhoneCard = ({ phone }) => {
-  
-  const { id, image, phone_name, brand_name } = phone || {};
+ 
+import PropTypes from 'prop-types';
 
-  const handleAddToFavorits = () => {
-    const addedFavoriteArray = [];
-    const favoritesItems = JSON.parse(localStorage.getItem("favorits"));
-    if (!favoritesItems) {
-      addedFavoriteArray.push(phone);
-      localStorage.setItem("favorits", JSON.stringify(addedFavoriteArray));
-      swal("Good job!", "product added successfuly!", "success");
-    } else {
-      const isExist = favoritesItems.find((phone) => phone.id === id);
-      if (!isExist) {
-        addedFavoriteArray.push(...favoritesItems, phone);
-        localStorage.setItem("favorits", JSON.stringify(addedFavoriteArray));
-        swal("Good job!", "product added successfuly!", "success");
-      }
-      else {
-        swal("Error!", "Already Added!", "error");
-      }
-    }
-  };
-
-  return (
-    <div className="flex justify-center items-center h-[80vh]">
+const FavoritesCard = ({ phone }) => {
+    const {image,brand_name,phone_name, } = phone || {}
+    return (
+        <div className="flex justify-center items-center  ">
       <div className="relative flex w-full max-w-[48rem] flex-row rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
         <div className="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
           <img src={image} alt="image" className="h-full w-full object-cover" />
@@ -40,7 +19,7 @@ const PhoneCard = ({ phone }) => {
 
           <a className="inline-block" href="#">
             <button
-              onClick={handleAddToFavorits}
+              
               className="flex select-none items-center gap-2 rounded-lg py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-pink-500 transition-all hover:bg-pink-500/10 active:bg-pink-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
               type="button"
             >
@@ -65,10 +44,11 @@ const PhoneCard = ({ phone }) => {
         </div>
       </div>
     </div>
-  );
+    );
 };
 
-PhoneCard.propTypes = {
-  phone: PropTypes.object,
+FavoritesCard.propTypes = {
+    phone:PropTypes.object
 };
-export default PhoneCard;
+
+export default FavoritesCard;
